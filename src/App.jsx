17429058f66f3d8f0a73cc0ef7cf79d3255a74f1
PabloEscales <pabloescales@gmail.com>
2023-10-20 15:20:45 +0200

    testing cli

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// initial commit test ahora! testing
+
 function App() {
   const [count, setCount] = useState(0)
 
