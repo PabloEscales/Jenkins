56978a14efb537fb487ab5ed94ab6d90adc044ef
poel <pablo.escales@gft.com>
2023-11-02 15:04:55 +0100

    66

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>665 && Docker 2023</h1>
+      <h1>Running in Docker and Jenkins</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
