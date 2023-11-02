c13fc19cdbe14ea35bfec069c650d17564da1f18
poel <pablo.escales@gft.com>
2023-11-02 10:10:52 +0100

    test1

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Pipeline && Jenkins PAUUUU</h1>
+      <h1>Jenkins pipeline</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
